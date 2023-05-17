51367c1d624b517dbbfcb1bdce550668a34ff1c0
Punkintosh <128147625+punkintosh@users.noreply.github.com>
2023-05-17 11:01:10 +0530

    Add pods with Snapkit

--- TestApp/HomeScreen.swift
+++ TestApp/HomeScreen.swift
@@ -6,15 +6,16 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeScreen: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 17/255, green: 17/255, blue: 17/255, alpha: 1.0)
         
         let helloLabel = UILabel()
-        helloLabel.text = "Hello, World!"
+        helloLabel.text = "Hello, World! 2"
         helloLabel.textAlignment = .center
         helloLabel.textColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1.0)
         helloLabel.font = UIFont.boldSystemFont(ofSize: 24)
@@ -22,11 +23,10 @@ class HomeScreen: UIViewController {
         
         view.addSubview(helloLabel)
         
-        NSLayoutConstraint.activate([
-            helloLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            helloLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-        ])
-    
+        helloLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview()
+        }
     }
-
 }
+
